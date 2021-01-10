fd4ad4aad28ae31d35f5c0a1f6fa564288057475
simmnu <66460972+simmnu@users.noreply.github.com>
2021-01-10 22:26:14 +0530

    update2

--- action.php
+++ action.php
@@ -8,12 +8,12 @@ if(isset($_POST['first_name'])){
 	$type=$_POST['type'];
     $message=$_POST['message'];
 
-	$to='aimmanuel85@gmail.com';
+	   $to='aimmanuel85@gmail.com';
     $esubject="Form submission";
     $content="Name : ".$first_name. "\n"."Email :" .$email."\n"."Subject :" .$type."\n"."Message"."\n\n".$message;
     $headers="From : " .$email;
 
-
+    $mail = new PHPMailer();
     if(mail($to,$esubject,$content,$headers))
     {
         echo "<h1> Sent Successfully! Thank You"."".$name."\n"."Bye</h1>";
@@ -22,6 +22,6 @@ if(isset($_POST['first_name'])){
     {
         echo "Something went wrong";
     }
-   
+
 }
 ?>
